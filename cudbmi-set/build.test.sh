22fe8d32af0a5b99217b52c9a159d267861a9abe
d33bs <dave.bunten@cuanschutz.edu>
2023-11-30 16:14:12 -0700

    mv to diff and patch fixes for setup-kg2-build.sh
    Co-Authored-By: Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>

--- cudbmi-set/build.test.sh
+++ cudbmi-set/build.test.sh
@@ -8,11 +8,15 @@ set -e
 # cd to the home dir for ubuntu user
 cd /home/ubuntu
 
-# insert fixes into line 102 of /home/ubuntu/RTX-KG2/setup-kg2-build.sh
-# for correcting python build requirements
-sudo sed -i.bak \
-    '102r /home/ubuntu/RTX-KG2/cudbmi-set/python-pydantic-core-workarounds.sh' \
-    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+# patch in fixes for setup-kg2-build.sh:
+# --------------------------------------
+# - insert fixes into line 102 of /home/ubuntu/RTX-KG2/setup-kg2-build.sh
+#   for correcting python build requirements
+# - replace archived postgres apt link in setup file
+#   - see: https://www.postgresql.org/message-id/ZN4OigxPJA236qlg%40msg.df7cb.de
+# - start postgresql service in order to run subsequent commands
+sudo patch -p1 /home/ubuntu/RTX-KG2/setup-kg2-build.sh \
+    /home/ubuntu/RTX-KG2/cudbmi-set/setup-kg2-build.intended.sh.patch
 
 # prepend python dependencies for amending potential challenges
 (cat /home/ubuntu/RTX-KG2/cudbmi-set/requirements-build-append.txt \
@@ -21,15 +25,6 @@ sudo sed -i.bak \
 
 # redefine s3_cp_cmd after it is set to avoid non-AWS centric work
 sudo sed -i.bak '11s/.*/s3_cp_cmd="echo"/' /home/ubuntu/RTX-KG2/master-config.shinc
-
-# replace archived postgres apt link in setup file
-# see: https://www.postgresql.org/message-id/ZN4OigxPJA236qlg%40msg.df7cb.de
-sudo sed -i.bak \
-    's|http://apt.postgresql.org/pub/repos/apt|https://apt-archive.postgresql.org/pub/repos/apt|g' \
-    /home/ubuntu/RTX-KG2/setup-kg2-build.sh
-
-# start postgresql service in order to run subsequent commands
-sudo sed -i.bak '174s/.*/&\n    sudo service postgresql start/' /home/ubuntu/RTX-KG2/setup-kg2-build.sh
 
 # run the build script, continuing even if there are errors
 sudo bash -x /home/ubuntu/RTX-KG2/setup-kg2-build.sh || true
